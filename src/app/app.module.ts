bdf782e09ee848bfb361ba73d97e557c85274cc2
Yong Wei <yongwei.goh@ncs.com.sg>
2023-03-26 18:59:39 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* new form imports */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyAuthenticatorModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+/// <reference types="@angular/localize" />
+import './polyfills.ts';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { Amplify } from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
